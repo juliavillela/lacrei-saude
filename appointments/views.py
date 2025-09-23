e2e723476b2067f8f029491fe809b42b1dadb537
juliavillela <julia.arruda@gmail.com>
2025-09-23 17:58:24 -0300

    style: apply code formatting and lint fixes

--- appointments/views.py
+++ appointments/views.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Appointment
 from .serializers import AppointmentSerializer
