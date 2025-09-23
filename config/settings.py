b6105fbea841eb1742017ed58cbaf8a857b6763a
juliavillela <julia.arruda@gmail.com>
2025-09-23 17:55:04 -0300

    fix: remove session authentication from RF authentication classes.

--- config/settings.py
+++ config/settings.py
@@ -100,7 +100,6 @@ AUTH_USER_MODEL = "accounts.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]
 }
