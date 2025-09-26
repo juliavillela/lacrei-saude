cf0009a5871ba94a5e8e10a31f5a2da621f5f15e
juliavillela <julia.arruda@gmail.com>
2025-09-26 11:34:54 -0300

    feat: enable session authentication and add login route

--- config/settings.py
+++ config/settings.py
@@ -108,6 +108,7 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 20,
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
+        'rest_framework.authentication.SessionAuthentication',
     ],
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
